16872bef0efbd11d312cd81611c4ed395129c56e
Rodrigo Vaz <rodrigodp2014@gmail.com>
2020-10-15 22:21:01 -0300

    Editing implementation

---
+++ Mvc_FormCollection/Controllers/HomeController.cs
@@ -0,0 +1,43 @@
+﻿using BLL;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Mvc;
+
+namespace Mvc_FormCollection.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            AlunoBLL _aluno = new AlunoBLL();
+            List<Aluno> alunos = _aluno.GetAlunos().ToList();
+
+            return View(alunos);
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(Aluno aluno)
+        {
+            if (ModelState.IsValid)
+            {
+                AlunoBLL alunoBll = new AlunoBLL();
+                alunoBll.IncluirAluno(aluno);
+                return RedirectToAction("Index");
+            }
+
+            return View();
+        }
+
+        public ActionResult Edit(int id)
+        {
+            AlunoBLL alunoBLL = new AlunoBLL();
+            Aluno aluno = alunoBLL.GetAlunos().Single(a => a.Id == id);
+            return View(aluno);
+        }
+    }
+}
